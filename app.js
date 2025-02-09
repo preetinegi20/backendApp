8e808974af8d8a9247e30fc84ac819b2780828a3
preetinegi20 <negipreeti4245@gmail.com>
2025-02-09 23:45:26 +0530

    Fix: Force reinstall dependencies

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://frontend-ev2fqp3nl-preetinegi20s-projects.vercel.app"],
     credentials: true, // Allow credentials (cookies, authorization headers)
   })
 );
